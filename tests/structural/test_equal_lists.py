56acb9b7b5bb11cdb98eed04aa1b59f20a33858a
Gustavo Felipe <gustavofeliperepositorio@gmail.com>
2024-03-20 09:19:41 -0300

    fix: test failed

--- tests/structural/test_equal_lists.py
+++ tests/structural/test_equal_lists.py
@@ -1,5 +1,5 @@
 def test_equal_lists():
     expected_list = [1, 2, 3, 4, 5]
-    result_list = [1, 2, 3, 4]
+    result_list = [1, 2, 3, 4, 5]
 
     assert result_list == expected_list, "The lists aren't equals"
